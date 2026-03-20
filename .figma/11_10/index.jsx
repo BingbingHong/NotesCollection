844006829021e2666ff3d530540b0772b0dec200
bingbinghong <bingbinghong@bingbinghongdeMacBook-Pro.local>
2026-03-20 16:16:04 +0800

    style: update icons to use Figma SVGs

---
+++ .figma/11_10/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.button}>
+      <img src="../image/mmym5qxy-kyv4xbf.svg" className={styles.icon} />
+    </div>
+  );
+}
+
+export default Component;
---
+++ .figma/21_12/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <img src="../image/mmym5wwb-c4w1adc.svg" className={styles.icon} />
+  );
+}
+
+export default Component;
